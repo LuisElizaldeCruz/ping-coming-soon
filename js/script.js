2edba77687d04373ad49623696c4e93e8f501462
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-06-21 23:58:01 -0500

    se agrego la validacion preeliminar del correo

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let form = document.querySelector(".frm");
+let input = document.querySelector(".frm__input");
+
+let mensaje = document.createElement("p");
+mensaje.className = "mensaje";
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(input.value);
+
+    if (input.value === "") {
+        // console.log("introduce un correo valido");
+        mensaje.textContent = "introduce un correo valido";
+        form.insertAdjacentElement("afterend", mensaje);
+        mensaje.style.setProperty("color", "red");
+        mensaje.style.setProperty("margin", "0 auto 10px 0");
+        input.style.setProperty("border-color", "red");
+
+        if (window.matchMedia("(max-width: 400px)").matches) {
+            input.insertAdjacentElement("afterend", mensaje);
+        }
+    }
+    else {
+        let nodo = document.querySelector(".mensaje");
+        nodo.remove();
+        input.style.setProperty("border-color", "hsl(223, 100%, 88%)");
+    }
+})
